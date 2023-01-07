404205ac5eb6c4b70ff93c4388ea39299d9a25d2
= <srinivasrpawar@gmail.com>
2023-01-07 20:34:24 +0530

    class 04 notes and creating food app

--- App.js
+++ App.js
@@ -1,2 +1,20 @@
 import react from "react";
-import reactDom from "react-dom";
+import ReactDOM, { createRoot } from "react-dom/client";
+const Title = () => {
+  <h1 id="title" key="h2">
+    Namaste React
+  </h1>;
+};
+
+const HeaderComponent = () => {
+  return (
+    <div>
+      <Title />
+      <h2>Namaste React functional component</h2>
+      <h2>This is a h2 Tag</h2>
+    </div>
+  );
+};
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<HeaderComponent />);
